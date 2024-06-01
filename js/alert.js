75f745ec6f6250214fe3b6b479b10432efab2b73
PalcivaPapricka <92027322+PalcivaPapricka@users.noreply.github.com>
2024-06-01 23:55:53 +0200

    Delete js directory

--- js/alert.js
+++
@@ -1,8 +0,0 @@
-
-//funkcia pre zavolanie alertu pri kliknuti na button
-const $but = document.querySelector(".jsklas");
-const $butbut = $but.querySelector("button");
-
-$butbut.addEventListener("click", () => {
-	alert('Vaša prihláška bola zaslaná');
-});
